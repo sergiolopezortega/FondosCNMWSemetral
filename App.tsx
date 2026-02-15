a5562e3216fa51f94d272aba990b72bbc1af52bb
Sergio <sergiolopezortega@outlook.com>
2026-02-15 12:01:33 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@ const App: React.FC = () => {
     setLoadError(null);
     
     const pathsToTry = [
-      '/fondosfiles.zip'
+      'fondosfiles.zip'
     ];
     
     setTriedPaths(pathsToTry);
